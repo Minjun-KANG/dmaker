2ad291705fc3e3970be7b3f29e370cb190261821
Minjun-KANG <inventer@kakao.com>
2022-02-24 11:23:25 +0900

    initial commit

---
+++ src/main/java/com/fastcampus/programming/dmaker/entitiy/Developer.java
@@ -0,0 +1,41 @@
+package com.fastcampus.programming.dmaker.entitiy;
+
+
+import com.fastcampus.programming.dmaker.type.DeveloperLevel;
+import com.fastcampus.programming.dmaker.type.DeveloperSkillType;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Developer {
+    //entity이기 때문에, @Id, @Gene..., Protected Long id와 같은 규약이 존재한다.
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    protected Long id;
+
+    @Enumerated(EnumType.STRING)
+    private DeveloperLevel developerLevel;
+
+    @Enumerated(EnumType.STRING)
+    private DeveloperSkillType developerSkillType;
+
+    private Integer experienceYears;
+    private String memberId;
+    private String name;
+    private Integer age;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
---
+++ src/main/java/com/fastcampus/programming/dmaker/repository/DeveloperRepository.java
@@ -0,0 +1,8 @@
+package com.fastcampus.programming.dmaker.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class DeveloperRepository {
+
+}
