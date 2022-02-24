ac1276b0ef6b3e6e928feac33513a1b72f3e7960
Minjun-KANG <inventer@kakao.com>
2022-02-24 10:59:42 +0900

    initial commit

---
+++ src/main/java/com/fastcampus/programming/dmaker/controller/DMakerController.java
@@ -0,0 +1,10 @@
+package com.fastcampus.programming.dmaker.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+//사용자 입력이 최초로 받아들여지는 Controller
+public class DMakerController {
+
+}
