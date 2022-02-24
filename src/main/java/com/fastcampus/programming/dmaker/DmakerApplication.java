6a27eb5e520080dcf9f397ed3bd6bf10a639d086
Minjun-KANG <inventer@kakao.com>
2022-02-24 11:23:16 +0900

    initial commit

--- src/main/java/com/fastcampus/programming/dmaker/DmakerApplication.java
+++ src/main/java/com/fastcampus/programming/dmaker/DmakerApplication.java
@@ -1,8 +1,11 @@
 package com.fastcampus.programming.dmaker;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class DmakerApplication {
 
--- src/main/java/com/fastcampus/programming/dmaker/controller/DMakerController.java
+++ src/main/java/com/fastcampus/programming/dmaker/controller/DMakerController.java
@@ -1,10 +1,23 @@
 package com.fastcampus.programming.dmaker.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import java.util.Arrays;
+import java.util.List;
 
+@RestController
+@Slf4j
 //사용자 입력이 최초로 받아들여지는 Controller
 public class DMakerController {
+
+    @GetMapping("/developers")
+    public List<String> getAllDevelopers() {
+        // GET /developers HTTP/1.1
+      log.info("GET /developers HTTP/1.1");
+
+      return Arrays.asList("snow", "Elsa", "Olaf");
+    }
 
 }
